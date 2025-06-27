761294cedc8088c3eedcc269c8b0dd5b6beafe9f
soorya-5002 <sooryakanthramki@gmail.com>
2025-06-27 15:00:36 +0530

    g

---
+++ Android/settings.gradle.kts
@@ -0,0 +1,22 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+    plugins {
+        id("com.android.application") version "8.4.0" apply false
+        id("org.jetbrains.kotlin.android") version "1.9.23" apply false
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Ohmies_App"
+include(":app")
